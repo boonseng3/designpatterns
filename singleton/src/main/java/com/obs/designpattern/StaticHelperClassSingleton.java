13c2f234b4c29e05bc027374f0cf96430e9e5e25
Boon Seng <boon-seng.ong@hpe.com>
2017-11-16 11:49:31 +0800

    Update comments

--- singleton/src/main/java/com/obs/designpattern/StaticHelperClassSingleton.java
+++ singleton/src/main/java/com/obs/designpattern/StaticHelperClassSingleton.java
@@ -3,10 +3,13 @@ package com.obs.designpattern;
 /**
  * Created by ongbo on 2/15/2017.
  * Thread safe and lazy loading.
+ * Initialization-on-demand holder idiom
+ * https://en.wikipedia.org/wiki/Initialization-on-demand_holder_idiom
  *
  */
 public class StaticHelperClassSingleton {
     private StaticHelperClassSingleton() {
+        System.out.println("instantiate  StaticHelperClassSingleton");
     }
 
     public static StaticHelperClassSingleton getInstance() {
